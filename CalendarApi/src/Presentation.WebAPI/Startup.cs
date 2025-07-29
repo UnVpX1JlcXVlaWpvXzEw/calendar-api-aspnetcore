399f39ec4e07848bd05fc47e5a990dc6afe1dc4d
RRequeijo <al79138@alunos.utad.pt>
2025-07-29 14:34:46 +0100

    [HABE-86] After CR comments

--- CalendarApi/src/Presentation.WebAPI/Startup.cs
+++ CalendarApi/src/Presentation.WebAPI/Startup.cs
@@ -2,5 +2,20 @@
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
+        public void Configure(WebApplication app)
+        {
+
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
