d35e1a88c41e3454e1580dae76810614dd54db31
RRequeijo <al79138@alunos.utad.pt>
2025-07-29 15:27:01 +0100

    [HABE-87] Fixes after CR

--- CalendarApi/src/Domain/Configuration/ServiceCollection.cs
+++ CalendarApi/src/Domain/Configuration/ServiceCollection.cs
@@ -2,13 +2,10 @@
 
 namespace HustleAddiction.Platform.CalendarApi.Domain.Configuration
 {
-
     public static class ServiceCollection
     {
-
         public static void RegisterDomainServices(this IServiceCollection services)
         {
-
         }
     }
 }
