1cf6bf51d4dcf5c528150ff9ca9dc66044485697
RRequeijo <al79138@alunos.utad.pt>
2025-07-29 20:36:53 +0100

    [HABE-90 & 91 & 92] Implement API domain entities and Add entities repositories interfaces and Implement entities repositories classes

---
+++ CalendarApi/src/Infrastructure/Repository/EventRepository.cs
@@ -0,0 +1,38 @@
+﻿using HustleAddiction.Platform.CalendarApi.Domain;
+using HustleAddiction.Platform.CalendarApi.Domain.Aggregate.Calendar;
+using HustleAddiction.Platform.CalendarApi.Domain.Aggregate.Calendar.Repository;
+using HustleAddiction.Platform.CalendarApi.Domain.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace HustleAddiction.Platform.CalendarApi.Infrastructure.Repository
+{
+    public class EventRepository : GenericRepository<Event>, IEventRepository
+    {
+        public EventRepository(CalendarAPIDbContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<Event>> GetByOwnerIdAsync(
+            Guid ownerId,
+            CancellationToken token)
+        {
+            return await this.Entities
+                .Where(e => e.OwnerId == ownerId)
+                .ToListAsync(token);
+        }
+
+        public async Task<IEnumerable<Event>> GetEventsInRangeAsync(
+            Guid ownerId,
+            DateTime start,
+            DateTime end,
+            CancellationToken token)
+        {
+            return await this.Entities
+                .Where(
+                    e => e.OwnerId == ownerId &&
+                    e.StartTime >= start &&
+                    e.EndTime <= end)
+                .ToListAsync(token);
+        }
+    }
+}
