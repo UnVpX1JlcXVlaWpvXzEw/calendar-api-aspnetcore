9ff36b696b5dd1a78dad1be3b704b56d0bc4b7ad
RRequeijo <al79138@alunos.utad.pt>
2025-08-04 13:36:27 +0100

    HABE-100 WIP

---
+++ CalendarApi/src/Presentation.WebAPI/Services/DeleteCalendar/DeleteCalendar.cs
@@ -0,0 +1,17 @@
+﻿namespace HustleAddiction.Platform.CalendarApi.Presentation.WebAPI.Services.DeleteCalendar
+{
+    using HustleAddiction.Platform.CalendarApi.Domain.Aggregate.Calendar.Repository;
+    using HustleAddiction.Platform.CalendarApi.Presentation.WebAPI.Tools.CurrentUserInfoProvider;
+
+    public class DeleteCalendar
+    {
+        private readonly ICalendarRepository calendarRepository;
+        private readonly ICurrentUserInfoProvider currentUserInfoProvider;
+
+        public DeleteCalendar(IServiceProvider provider)
+        {
+            calendarRepository = provider.GetRequiredService<ICalendarRepository>();
+            currentUserInfoProvider = provider.GetRequiredService<ICurrentUserInfoProvider>();
+        }
+    }
+}
---
+++ CalendarApi/src/Presentation.WebAPI/Services/DeleteCalendar/IDeleteCalendar.cs
@@ -0,0 +1,7 @@
+﻿namespace HustleAddiction.Platform.CalendarApi.Presentation.WebAPI.Services.DeleteCalendar
+{
+    public interface IDeleteCalendar
+    {
+        Task<Guid> DeleteAsync(Guid guid, Guid ownerId, CancellationToken cancellationToken);
+    }
+}
