0911d5f445f22366c64eccb7e5e67369feb9e889
RRequeijo <al79138@alunos.utad.pt>
2025-08-25 15:48:44 +0100

    [HABE-117] Fixes after CR

--- CalendarApi/src/Presentation.WebAPI/Configuration/ServiceCollection.cs
+++ CalendarApi/src/Presentation.WebAPI/Configuration/ServiceCollection.cs
@@ -7,7 +7,6 @@
     using HustleAddiction.Platform.CalendarApi.Presentation.WebAPI.Services.DeleteReminder;
     using HustleAddiction.Platform.CalendarApi.Presentation.WebAPI.Services.GetCalendar;
     using HustleAddiction.Platform.CalendarApi.Presentation.WebAPI.Services.GetEventByCalendar;
-    using HustleAddiction.Platform.CalendarApi.Presentation.WebAPI.Services.ScheduleNotificationJob;
     using HustleAddiction.Platform.CalendarApi.Presentation.WebAPI.Services.UpdateEvent;
     using HustleAddiction.Platform.CalendarApi.Presentation.WebAPI.Tools.CurrentUserInfoProvider;
     using HustleAddiction.Platform.CalendarApi.Presentation.WebAPI.Tools.DateTimeProvider;
@@ -37,8 +36,7 @@
                 .AddScoped<IDeleteEvent, DeleteEvent>()
                 .AddScoped<IUpdateEvent, UpdateEvent>()
                 .AddScoped<IDeleteReminder, DeleteReminder>()
-                .AddScoped<IGetEventByCalendar, GetEventByCalendar>()
-                .AddScoped<IScheduleNotificationJob, ScheduleNotificationJob>();
+                .AddScoped<IGetEventByCalendar, GetEventByCalendar>();
         }
     }
 }
